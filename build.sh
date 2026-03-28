a55d9f0cae50eba1a43e192e6a6690907106589b
Your Name <your@email.com>
2026-03-28 14:24:41 +0530

    fixed build.sh commands

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py migrateectstatic --noinput
+python manage.py migrate
+python manage.py collectstatic --noinputoinput

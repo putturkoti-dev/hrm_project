83e22cb67bf7d486c45c4743594bc8a2f677e98c
Your Name <your@email.com>
2026-03-28 14:49:46 +0530

    auto create superuser

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@
 pip install -r requirements.txt
 
 python manage.py migrate
+
+python manage.py createsuperuser \
+  --noinput \
+  --username admin \
+  --email admin@gmail.com || true
+
 python manage.py collectstatic --noinput
